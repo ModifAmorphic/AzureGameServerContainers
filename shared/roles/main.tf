cfee3e875452e51909f3a03ccc306adec0e1a17e
ModifAmorphic <ModifAmorphic@gmail.com>
2024-02-06 19:51:06 -0700

    Workflow for terraforming shared resources

--- shared/roles/main.tf
+++ shared/roles/main.tf
@@ -1,24 +1,41 @@
+data "azurerm_subscription" "current" {
+}
+
 data "azurerm_resource_group" "gaming" {
   name     = "gaming"
 }
 
-data "azurerm_subscription" "primary" {
+resource "azurerm_role_definition" "container_manager_role" {
+  name        = "Container Instance Manager"
+  scope       = data.azurerm_subscription.current.id
+  description = "Allow full access to the Azure Container Instance container group resources. Custom role created with Terraform."
+
+  permissions {
+    actions     = [
+      "Microsoft.ContainerInstance/containerGroups/*",
+      "Microsoft.Resources/subscriptions/resourcegroups/read"
+      ]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.current.id, # /subscriptions/00000000-0000-0000-0000-000000000000
+  ]
 }
 
-# resource "azurerm_role_definition" "storage_account_role" {
-#   name        = "Storage Account Manager"
-#   scope       = data.azurerm_subscription.primary.id
-#   description = "Allow full access to the Azure Container Instance container group resources. Custom role created with Terraform."
+resource "azurerm_role_definition" "vm_scheduler_role" {
+  name        = "Virtual Machine Scheduler"
+  scope       = data.azurerm_subscription.current.id
+  description = "Allows listing, starting and stopping of virtual machines. Custom role created with Terraform."
 
-#   permissions {
-#     actions     = [
-#       "Microsoft.ContainerInstance/containerGroups/*",
-#       "Microsoft.Resources/subscriptions/resourcegroups/read"
-#       ]
-#     not_actions = []
-#   }
+  permissions {
+    actions     = [
+      "Microsoft.Compute/virtualMachines/*"
+      ]
+    not_actions = []
+  }
 
-#   assignable_scopes = [
-#     data.azurerm_subscription.primary.id, # /subscriptions/00000000-0000-0000-0000-000000000000
-#   ]
-# }
+  assignable_scopes = [
+    data.azurerm_subscription.current.id, # /subscriptions/00000000-0000-0000-0000-000000000000
+  ]
+}
